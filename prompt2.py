a11dfa913e77a8cd718503d30facf67379180c77
jayritomusic <juceorti@ucsc.edu>
2024-10-02 18:58:25 -0700

    Prompt 2 Coding Journal #1

---
+++ prompt2.py
@@ -0,0 +1,18 @@
+#sum of two floating points
+x=0.5
+y=5.3
+z=(x + y)
+print(z)
+
+#difference of two integers
+r=int(52)
+s=int(13)
+t=(r-s)
+print(t)
+
+#product of floating point and integer
+u=9.72
+v=42
+w=(u * v)
+print(w)
+
